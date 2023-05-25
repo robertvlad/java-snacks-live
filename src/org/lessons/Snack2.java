2aa133a3f32d3da1bc614b934768acdb57337637
robertvlad <vladrob96@gmail.com>
2023-05-26 00:09:54 +0300

    Snack 2

--- src/org/lessons/Snack2.java
+++ src/org/lessons/Snack2.java
@@ -1,5 +1,38 @@
 package org.lessons;
 
+import java.util.Scanner;
+
 public class Snack2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Inserisci la prima parola");		
+		String p1 = sc.nextLine();
+		
+		System.out.println("Inserisci la seconda parola");		
+		String p2 = sc.nextLine();
+		
+		int p1lng = p1.length();
+		int p2lng = p2.length();
+		
+		if (p1lng == p2lng) {
+			
+			System.out.println("Le due parole hanno la stessa lunghezza");
+		}
+		else if (p1lng > p2lng) {
+			
+			System.out.println(p1);
+			System.out.println(p2);
+		}
+		else {
+			
+			System.out.println(p2);
+			System.out.println(p1);
+		}
+		
+		sc.close();
+	}
 
 }
