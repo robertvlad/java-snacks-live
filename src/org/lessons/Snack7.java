176d243a2f2ae446969cbed66a56ce81b8a916e7
robertvlad <vladrob96@gmail.com>
2023-05-26 15:47:50 +0300

    Snack 7

---
+++ src/org/lessons/Snack7.java
@@ -0,0 +1,15 @@
+package org.lessons;
+
+public class Snack7 {
+	
+	public static void main(String[] args) {
+		
+		int i = 2;
+		
+		while (i < 1000) {
+			
+			i *= 2;
+			System.out.println(i);
+		}
+	}
+}
